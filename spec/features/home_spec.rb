51ea99faeb73d5d4605ef8851c3742f72c0a937b
Pirun Seng <sengpirun.rain@gmail.com>
2017-12-08 15:31:27 +0700

    Remove authenticate user from ApplicationController class

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,16 +1,8 @@
 describe 'Home' do
-  let(:user){ create(:user) }
-
-  feature 'login' do
-    scenario 'failed' do
+  feature 'Home page' do
+    before do
       visit root_path
-      expect(page).to have_content('You need to sign in or sign up before continuing.')
     end
-
-    scenario 'success' do
-      login_as(user)
-      visit root_path
-      expect(page).not_to have_content('You need to sign in or sign up before continuing.')
-    end
+    it { expect(page).to have_content('Hello World') }
   end
 end
